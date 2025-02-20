d1293a31c536f4196e05fca18a1c6d22a5bae8ac
GitHub Action <action@github.com>
2025-02-20 11:15:07 +0000

    android-[2025/02/20]-10831

--- android/Exports/States/SD_m1002003.lua
+++ android/Exports/States/SD_m1002003.lua
@@ -1,7 +1,7 @@
 --状态数据
 local this = 
 {
-eff_addition_pack={"m10020","common_hit"},
+eff_addition_pack={"m10020","common_hit"},no_eff_ab=1,
 summon={play_time=7467,hide_scene=1,skill_scene="black",hide_buff=1,summon_delay=0},
 cast2={play_time=4500,hide_scene=1,hide_buff=1,showDatas={{delay=4000,showState=1,isEnemy=1},{delay=0,isEnemy=1},{delay=0,isFriend=1}}},
 cast1={play_time=2000},
